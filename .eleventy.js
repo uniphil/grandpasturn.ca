dd75c98d430f8877d5008186c59041d71cd035d1
phil <uniphil@gmail.com>
2023-02-20 14:17:15 -0500

    add bowls json to static files

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('static');
+  eleventyConfig.addPassthroughCopy('static', { expand: true });
   eleventyConfig.setServerPassthroughCopyBehavior('passthrough');
   return { dir: { input: 'pages' } };
 };
