5ab95783117b6c87507f25abd94c8b337b8b8fe7
phil <uniphil@gmail.com>
2023-02-23 22:15:14 -0500

    add esbuild for overlay/preview app thing

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,16 @@
-module.exports = function(eleventyConfig) {
+const esbuild = require('esbuild');
+
+module.exports = eleventyConfig => {
   eleventyConfig.addPassthroughCopy('static', { expand: true });
   eleventyConfig.setServerPassthroughCopyBehavior('passthrough');
+  eleventyConfig.addWatchTarget('./overlay/');
+  eleventyConfig.on('afterBuild', () => esbuild.build({
+    entryPoints: ['overlay/index.js'],
+    outfile: '_site/static/overlay.js',
+    bundle: true,
+    target: 'es6',
+    minify: process.env.ELEVENTY_ENV === 'production',
+    sourcemap: true
+  }));
   return { dir: { input: 'pages' } };
 };
