120087ba709d136b45751833b992e42a88195d7d
phil <uniphil@gmail.com>
2023-02-25 01:48:48 -0500

    some bowl info

--- overlay/index.jsx
+++ overlay/index.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import bowls from '../pages/_data/bowls.json';
+
+const numToBowl = bowls.reduce((a, b) => {
+  a[b.number] = b;
+  return a;
+}, {});
+
 
 class BowlOverlay extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {};
-    console.log('heyo', props.num)
-  }
   esc = () => window.location.hash = '';
   stop = e => e.stopPropagation();
   render() {
+    const bowl = numToBowl[this.props.num];
     return (
       <div className="bowl-overlay" onClick={this.esc}>
         <div className="overlay-content" onClick={this.stop}>
           <h2>Bowl #{this.props.num}</h2>
-          <img height="200" width="300" alt="" />
-          <a class="button" href={this.props.href}>
-            Read more&hellip;
-          </a>
+          <div className="bowl-detail flow left">
+            <div className="bowl-images">
+              <img height="200" width="300" alt="" />
+            </div>
+            <div className="bowl-info flow">
+              <p>{bowl.description}</p>
+              <p>Completed {bowl.date}</p>
+              <ul className="actual">
+                <li>{bowl.source}</li>
+                <li>{bowl.feature}</li>
+              </ul>
+              <a className="button" href={this.props.href}>
+                Read more&hellip;
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     );
@@ -35,7 +50,6 @@ const root = ReactDOM.createRoot(document.getElementById('hi-app'));
 function nav(u) {
   const num = u.hash.split('#')[1];
   const el = document.querySelector(`[data-overlay-bowl='${num}']`);
-  console.log({num, el});
   if (num === undefined || el === undefined) {
     root.render(<></>);
   } else {
