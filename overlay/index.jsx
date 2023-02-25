1cb4bbc03debca3e3c5922d06011bba9783d5857
phil <uniphil@gmail.com>
2023-02-24 23:07:08 -0500

    hacky hash router

--- overlay/index.jsx
+++ overlay/index.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 class Overlay extends React.Component {
+  constructor(props) {
+    super();
+  }
   render() {
     return <div>test test... bowl #{this.props.bowl}</div>;
   }
 }
 
-ReactDOM
-    .createRoot(document.getElementById('hi-app'))
-    .render(<Overlay bowl="1" />);
+
+Array.from(document.querySelectorAll('[data-overlay-bowl]')).forEach(el => {
+  el.setAttribute('data-permalink', el.href);
+  el.href = `#${el.getAttribute('data-overlay-bowl')}`
+});
+
+const hashnum = u => u.hash.split('#')[1]
+const root = ReactDOM.createRoot(document.getElementById('hi-app'));
+root.render(<Overlay bowl={hashnum(window.location)} />);
+addEventListener('hashchange', e =>
+  root.render(<Overlay bowl={hashnum(new URL(e.newURL))} />));
