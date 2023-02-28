c9302be7646756c23ddc74e31366cc6c5d5f78d1
phil <uniphil@gmail.com>
2023-02-28 17:02:29 -0500

    make images kinda work

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,25 @@
+const Image = require('@11ty/eleventy-img');
 const esbuild = require('esbuild');
 
+async function imageShortcode(src, alt, widths, sizes, className='') {
+  const w = widths.concat(widths.map(w => w * 2));
+  console.log({ w });
+  const meta = await Image(src, {
+    outputDir: '_site/static/images',
+    urlPath: '/static/images',
+    widths: w,
+    formats: ['avif', 'jpeg', 'webp'],
+    sharpAvifOptions: { quality: 75 },
+    sharpJpegOptions: { quality: 92 },
+    sharpWebpOptions: { quality: 92 },
+  });
+  return Image.generateHTML(meta, { alt, sizes, 'class': className });
+}
+
 module.exports = eleventyConfig => {
+  eleventyConfig.addAsyncShortcode('image', imageShortcode);
   eleventyConfig.addPassthroughCopy('static', { expand: true });
-  eleventyConfig.setServerPassthroughCopyBehavior('passthrough');
+  // eleventyConfig.setServerPassthroughCopyBehavior('passthrough');
   eleventyConfig.addWatchTarget('./overlay/');
   eleventyConfig.on('afterBuild', () => esbuild.build({
     entryPoints: ['overlay/index.jsx'],
