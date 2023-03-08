6a4367d934e59f5dc5dd371e70dd552bd1348b07
phil <uniphil@gmail.com>
2023-03-08 15:07:22 -0500

    leave bowl links as real links
    handle clicks with JS enabled via click handler instead of rewriting links to hash targets
    
    also ignores any clicks with modifier keys, so ctrl/shift+click still opens the detail page in a new tab/window.

--- overlay/index.jsx
+++ overlay/index.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import BowlOverlay from './BowlOverlay';
 
-Array.from(document.querySelectorAll('[data-overlay-bowl]')).forEach(el => {
-  el.setAttribute('data-permalink', el.href);
-  el.href = `#${el.getAttribute('data-overlay-bowl')}`
-});
-
 let _gct = 100,
     _gcq = [],
     _count = ({ path, n }) => goatcounter.count({ path, title: n ? `Bowl ${n}` : '', event: true });
@@ -22,19 +17,23 @@ const root = ReactDOM.createRoot(document.getElementById('overlay-app'));
 let _overlayState = null;
 function nav(u) {
   const num = u.hash.split('#')[1];
-  const el = document.querySelector(`[data-overlay-bowl='${num}']`);
+  const el = document.querySelector(`[data-bowl='${num}'] > a`);
   if (num === undefined || el === undefined) {
     root.render(<></>);
     if (_overlayState !== null) overlayEvent('overlay:closed');
     _overlayState = 'closed';
   } else {
-    const href = el.getAttribute('data-permalink');
-    root.render(<BowlOverlay num={num} href={href} />);
+    root.render(<BowlOverlay num={num} href={el.href} />);
     if (_overlayState === null) overlayEvent('overlay:loaded-open', num);
     else if (_overlayState === 'closed') overlayEvent('overlay:opened', num);
     else overlayEvent('overlay:changed', num);
     _overlayState = 'open';
   }
 }
+Array.from(document.querySelectorAll('[data-bowl] > a')).forEach(el => el.addEventListener('click', e => {
+  if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) return;
+  e.preventDefault();
+  window.location.hash = el.parentNode.getAttribute('data-bowl');
+}));
 nav(window.location);
 addEventListener('hashchange', e => nav(new URL(e.newURL)));
